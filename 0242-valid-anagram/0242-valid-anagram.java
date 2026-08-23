bafdf2bd4682ce783fe5808d257f24b9301f7ef5
Rehan Khan <102506646+rehankhan07@users.noreply.github.com>
2026-08-23 14:46:28 +0530

    Time: 17 ms (16.64%), Space: 46.4 MB (34.65%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //   if(s1.length()!=s2.length()) return false;
+        // char[] arr1=s1.toCharArray();
+        //  char[] arr2=s2.toCharArray();
+        //  Arrays.sort(arr1);
+        //  Arrays.sort(arr2);
+        //  for(int i=0;i<arr1.length;i++){
+        //      if(arr1[i]!=arr2[i]){
+        //          return false;
+        //      }
+        //  }
+        //  return true;
+          if (s.length()!=t.length())return false;
+        Map<Character, Integer> maps = new HashMap<>();
+        Map<Character, Integer> mapt = new HashMap<>();
+        for (int i=0;i<s.length();i++){
+            char q= s.charAt(i);
+            if (!maps.containsKey(q)) maps.put(q,1);
+            else {
+                maps.put(q, maps.get(q)+1);
+            }
+        }
+        for (int j=0;j<t.length();j++){
+            char w= t.charAt(j);
+            if (!mapt.containsKey(w)) mapt.put(w,1);
+            else {
+                mapt.put(w, mapt.get(w)+1);
+            }
+
+        }
+        for (char key : maps.keySet()) {
+            if (!mapt.containsKey(key) || !maps.get(key).equals(mapt.get(key))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
