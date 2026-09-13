77c4d7ad6f398abe5b335fb467415d5eaf4281ff
Rehan Khan <102506646+rehankhan07@users.noreply.github.com>
2026-09-13 17:27:17 +0530

    Time: 25 ms (37.3%), Space: 49.4 MB (8.74%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calculate(String s) {
+       Stack<Integer> st = new Stack<>();
+      int curr=0;
+      char op='+';
+      char[] ch = s.toCharArray();
+      for(int i=0;i<ch.length;i++){
+        if(Character.isDigit(ch[i])){
+            curr=curr*10+ch[i]-'0';
+        }
+        if(!Character.isDigit(ch[i]) && ch[i]!=' ' || i==ch.length-1){
+          if(op=='+') st.push(curr);
+          else if(op=='-') st.push(-curr);
+          else if (op=='*') st.push(st.pop()*curr);
+          else if (op=='/') st.push(st.pop()/curr);
+          op=ch[i];
+          curr=0;
+        }
+      }
+      int sum =0;
+      while(!st.isEmpty()){
+        sum+=st.pop();
+      }
+      return sum;
+    }
+}
