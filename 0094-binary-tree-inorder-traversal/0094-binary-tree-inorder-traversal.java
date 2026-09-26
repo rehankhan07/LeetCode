a912423e589eaaf1edada0754f702bad1f92d560
Rehan Khan <102506646+rehankhan07@users.noreply.github.com>
2026-09-26 18:02:57 +0530

    Time: 0 ms (100%), Space: 43.3 MB (15.41%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,22 +1,7 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
      public void dfs(TreeNode root, ArrayList<Integer> ans){
     if(root==null) return;
-   
     dfs(root.left,ans);
      ans.add(root.val);
     dfs(root.right,ans);
